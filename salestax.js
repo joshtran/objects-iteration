43438f43b572a8ec67d1c4452b7a295acf3189c0
joshtran <joshuatrain@gmail.com>
2017-01-12 03:36:52 +0000

    Committing completed salestax.js exercise.

--- salestax.js
+++ salestax.js
@@ -10,7 +10,7 @@ var companySalesData = [
     province: "BC",
     sales: [ 100, 200, 400 ]
   },
-    {
+  {
     name: "Bombardier",
     province: "AB",
     sales: [ 80, 20, 10, 100, 90, 500 ]
@@ -20,42 +20,48 @@ var companySalesData = [
     province: "SK",
     sales: [ 500, 100 ]
   }
-
 ];
 
-// console.log(companySalesData[0].sales[0]);
-function sum(list) {
-
-}
-
-function sumSales(companyName) {
-  // Loop through entire object
-  for (var i = 0; i < companySalesData.length; i++) {
-
-    // Find company's name
-    if (companySalesData[i].name === companyName) {
-
-      // Set variable to tabulate sales for company
-      var totalSales = 0;
-
-      //List sales
-      console.log(companyName + companySalesData[i].province + " these are the sales " + companySalesData[i].sales);
-
-      //Loop through all sales for company
-      for (var counter = 0; counter < companySalesData[i].sales.length; counter++) {
-
-        //Sum sales
-        totalSales = totalSales + companySalesData[i].sales[counter];
 
 
-      }
-
-        //Print results -- is this working?
-        console.log(companyName + companySalesData[i].province + " this is the sum of sales" + totalSales);
-
-
+function calculateSalesTax(salesData, taxRates) {
+  // Create new container object for companies
+  var outputObject = {};
+  // Loop through original object
+  for (var i = 0; i < salesData.length; i++) {
+    //Check to see if name key exists in new container object
+    if (outputObject[salesData[i].name] === undefined){
+      //Since name key not found in new container object, create nested company object
+      outputObject[salesData[i].name] = {
+      totalSales : 0,
+      totalTaxes : 0
+    }}
+    //Calculate total sales for company and plug into appropriate key in nested company object
+    outputObject[salesData[i].name].totalSales +=
+     sumArray(salesData[i].sales)
+    //Calculate total taxes for company and plug into appropriate key in nested company object
+    outputObject[salesData[i].name].totalTaxes +=
+     totalTax(sumArray(salesData[i].sales), salesData[i].province)
     }
-  }
+  return outputObject;
 }
 
-sumSales("Telus");
+
+
+// Calculate and ouput total taxes -- values passed in from given object key
+function totalTax(totalSale, province){
+  var taxRate = salesTaxRates[province];
+  output = totalSale * taxRate;
+  return output;
+}
+// Calculate and output total sales -- array passed in from given object key
+function sumArray(saleArray){
+  var total = 0;
+  for (var i = 0; i < saleArray.length; i++) {
+    total += saleArray[i];
+  }
+  return total;
+}
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+console.log(results);
